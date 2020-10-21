1ea396d0d4f4e08552316dd3c3a99be46f1361cd
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-21 17:41:28 -0300

    https://www.codewars.com/kata/58287977ef8d4451f90001a0/train/javascript

---
+++ CodingMeetup#6-Higher-OrderFunctionsSeries-CanTheyCodeInTheSameLanguage.js
@@ -0,0 +1,11 @@
+const list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+function isSameLanguage(list) {
+  return list.every(obj => list[0].language === obj.language);
+}
+
+console.log(isSameLanguage(list1));

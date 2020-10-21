8cbb24609d83112cb64e76be04e5fa0fe3d93687
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-21 18:21:47 -0300

    https://www.codewars.com/kata/582dace555a1f4d859000058/train/javascript

---
+++ CodingMeetup#12-Higher-OrderFunctionsSeries-FindGitHubAdmins.js
@@ -0,0 +1,12 @@
+const list1 = [
+  { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 22, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 49, language: 'Ruby', githubAdmin: 'no' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 34, language: 'JavaScript', githubAdmin: 'yes' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript', githubAdmin: 'no' }
+];
+
+function findAdmin(list, lang) {
+  return list.filter(obj => obj.githubAdmin === 'yes' && obj.language === lang);
+}
+
+console.log(findAdmin(list1, 'JavaScript'));

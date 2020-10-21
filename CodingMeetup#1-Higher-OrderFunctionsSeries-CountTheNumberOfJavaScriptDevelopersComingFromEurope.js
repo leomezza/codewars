4c556dbea49784dc3e8230286ba2b93d4e154744
Leonardo Mezzanotti <leomezza@gmail.com>
2020-10-20 21:59:00 -0300

    https://www.codewars.com/kata/582746fa14b3892727000c4f/train/javascript

---
+++ CodingMeetup#1-Higher-OrderFunctionsSeries-CountTheNumberOfJavaScriptDevelopersComingFromEurope.js
@@ -0,0 +1,15 @@
+const list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+function countDevelopers(list) {
+  return list.reduce((acc, curV) => {
+    if (curV.continent === 'Europe' && curV.language === 'JavaScript') acc += 1;
+    return acc;
+  }, 0);
+}
+
+console.log(countDevelopers(list1));
